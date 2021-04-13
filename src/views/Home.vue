e4d9aada563129f144eb0d3cf96b9e50a8340ad1
Emerson Pinheiro de Souza <emersonline2007@gmail.com>
2021-04-13 11:02:33 -0300

    Configuração e estilização do Header, Footer e primeiro componentes e início da página de busca

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,30 @@
 <template>
-  <div></div>
+  <section>
+    <h1>👍Compre ou Venda👌</h1>
+    <ProdutosBuscar />
+    <ProdutosLista />
+  </section>
 </template>
 
 <script>
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
+import ProdutosLista from "@/components/ProdutosLista.vue";
+
 export default {
   name: "Home",
+  components: {
+    ProdutosBuscar,
+    ProdutosLista
+  }
 };
 </script>
+
+<style scoped>
+  h1{
+    color: #87f;
+    font-size: 2rem;
+    margin-top: 40px;
+    text-align: center;
+
+  }
+</style>
