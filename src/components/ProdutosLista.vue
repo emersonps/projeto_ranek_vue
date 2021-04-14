ee8343f2405f9fd8a5d89ee75e8b197e5afbebde
Emerson Pinheiro de Souza <emersonline2007@gmail.com>
2021-04-14 09:26:52 -0300

    estilização dos produtos e correção de busca - produto não encontrado.

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,66 +1,99 @@
 <template>
-    <section class="produtos-container">
-      <div v-for="produto in produtos" :key="produto.id">
-        <img v-if="produtos.fotos" :src="produto.fotos[0].src" alt="produto.fotos[0].titulo">
-        <p class="preco">{{produto.preco}}</p>
-        <h2 class="titulo">{{produto.nome}}</h2>
-        <p>{{produto.descricao}}</p>
-        <!-- <p>{{url}}</p> -->
+  <section class="produtos-container">
+    <div v-if="produtos && produtos.length" class="produtos">
+      <div class="produto" v-for="produto in produtos" :key="produto.id">
+        <router-link to="/">
+          <img v-if="produtos.fotos" :src="produto.fotos[0].src" alt="produto.fotos[0].titulo"/>
+          <p class="preco">{{ produto.preco }}</p>
+          <h2 class="titulo">{{ produto.nome }}</h2>
+          <p>{{ produto.descricao }}</p>
+        </router-link>
       </div>
-    </section>
+    </div>
+    <div v-else-if="produtos && produtos.length === 0">
+      <p class="sem-resultados">Busca sem resultados. Tente buscar outro termo.</p>
+    </div>
+  </section>
 </template>
 
 <script>
-  import { api } from "@/services.js";
+import { api } from "@/services.js";
+import { serialize } from "@/helpers.js";
 
-  export default{
-    data(){
-      return{
-        produtos: null
-      }
+export default {
+  data() {
+    return {
+      produtos: null,
+      produtosPorPagina: 9,
+    };
+  },
+  computed: {
+    url() {
+      const query = serialize(this.$route.query);
+      return `/produto?_limit=${this.produtosPorPagina}${query}`;
     },
-    computed: {
-      url(){
-        //Serializar a informação
-        let queryString = "";
-        for (let key in this.$route.query){
-          queryString += `&${key}=${this.$route.query[key]}`;
-        }
-        // console.log(queryString);
-
-        // return this.$route.query
-        return "/produto?_limit=10" + queryString;
-      }
+  },
+  methods: {
+    getProdutos() {
+      api.get(this.url).then((response) => {
+        this.produtos = response.data;
+      });
     },
-    methods: {
-      getProdutos(){
-        // Axios - preferi substituir o uso do fetch pela biblioteca axios, pela facilidade que ele nos dá 
-        // na conversão dos dados para JSon (pois ele já faz isso automaticamente), e pela prática 
-        // nos Gets e Posts
-        api.get(this.url).then(response => {
-        // api.get(this.url).then(response => {
-          // console.log(response);
-          this.produtos = response.data;
-        });
-
-        // fetch("http://localhost:3000/produto")
-        // .then(response => response.json())
-        // .then(response => {
-        //   this.produtos = response;
-        // });
-      }
-    },
-    watch: {
-      url(){
-        this.getProdutos();
-      }
-    },
-    created() {
+  },
+  watch: {
+    url() {
       this.getProdutos();
-    }
-  };
+    },
+  },
+  created() {
+    this.getProdutos();
+  },
+};
 </script>
- 
-<style>
 
+<style scoped>
+  .produtos-container{
+    max-width: 1000px;
+    margin: 0 auto;
+  }
+  
+  .produtos{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 30px;
+    margin: 30px;
+  }
+
+  .produto{
+    box-shadow: 0 6px 12px rgba(30,60, 90, 0.1);
+    padding: 10px;
+    background: #fff;
+    border-radius: 4px;
+    transition: all 0.2s;
+  }
+
+  .produto:hover{
+      box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+      transform: scale(1.1);
+      position: relative;
+      z-index: 1;  
+  }
+
+  .produto img{
+    border-radius: 4px;
+    margin-bottom: 20px;
+  }
+
+  .titulo{
+    margin-bottom: 10px;
+  }
+
+  .preco{
+    color: #e80;
+    font-weight: bold;
+  }
+
+  .sem-resultados{
+    text-align: center;
+  }
 </style>
