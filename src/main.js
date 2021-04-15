1a2a4a4646f62de908084ef3aa27679294909247
Emerson Pinheiro de Souza <emersonline2007@gmail.com>
2021-04-14 23:36:25 -0300

    filter dos produtos - estilização dos produtos

--- src/main.js
+++ src/main.js
@@ -8,6 +8,20 @@ Vue.config.productionTip = false
 
 Vue.component("PaginaCarregando", PaginaCarregando);
 
+Vue.filter("numeroPreco", valor=>{
+  valor = Number(valor);
+  if(!isNaN(valor)){
+    return valor.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    });
+  }else{
+    return  ""
+  }
+  // console.log(valor);
+});
+
+
 new Vue({
   router,
   store,
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,27 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "../views/Home.vue";
+import Produto from "../views/Produto.vue";
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-]
-
-const router = new VueRouter({
-  mode: 'history',
+export default new Router({
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
-
-export default router
+  routes:[
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/produto/:id",
+      name: "produto",
+      component: Produto,
+      props: true
+    }
+  ],
+  scrollBehavior(){
+    return window.scrollTo({top: 0, behavior: "smooth"});
+  }
+});
