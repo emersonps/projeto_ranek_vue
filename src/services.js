2c2f6c8c7fde182b2c3f0685a3ab0f977790e259
Emerson Pinheiro de Souza <emersonline2007@gmail.com>
2021-04-15 19:34:42 -0300

    Rotina de alteração do usuário

--- src/services.js
+++ src/services.js
@@ -14,6 +14,9 @@ export const api = {
     //Passando o body (objeto), o axios transforma em Json e posta na API
     return axiosInstance.post(endpoint, body);
   },
+  put(endpoint, body){
+    return axiosInstance.put(endpoint, body);
+  },
   delete(endpoint){
     return axiosInstance.delete(endpoint);
   }
