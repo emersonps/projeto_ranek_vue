34f936bbc29105684fb098365489cb8df30a9be0
Emerson Pinheiro de Souza <emersonline2007@gmail.com>
2021-05-12 09:42:16 -0300

    Listagem de produtos comprados por cliente.

--- src/views/usuario/UsuarioCompras.vue
+++ src/views/usuario/UsuarioCompras.vue
@@ -1,13 +1,51 @@
 <template>
-  <p>Lista de Compras</p>
+  <section>
+    <div v-if="compras">
+      <h2>Compras</h2>
+      <div class="produtos-wrapper" v-for="(compra, index) in compras" :key="index">
+        <ProdutoItem v-if="compra.produto" :produto="compra.produto">
+          <p class="vendedor">
+            <span>Vendedor: </span> 
+            {{compra.vendedor_id}}
+          </p>
+        </ProdutoItem> 
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import ProdutoItem from "@/components/ProdutoItem.vue";
+import {api} from "@/services.js";
+import {mapState} from "vuex";
+
 export default {
-  name: "Usuario",
+  components: {
+    ProdutoItem
+  },
+  data(){
+    return {
+      compras: null,
+    };
+  }, 
+  computed: {
+    ...mapState(["usuario", "login"])
+  },
   methods: {
-    deslogar(){
-      
+    getCompras() {
+      api.get(`/transacao?comprador_id=${this.usuario.id}`).then(response => {
+        this.compras = response.data; 
+      });
+    }
+  },
+  watch: {
+    login(){
+      this.getCompras();
+    }
+  },
+  created(){
+    if(this.login){
+      this.getCompras();
     }
   }
 };
